2d124c8456fd7e93e5aa2e417006e1fbeee358fe
Jaskirat Singh <Jaskirat.Singh@microsoft.com>
2023-05-16 21:21:46 +0530

    Importing Paramiko

--- HttpTriggerSAPKerUpg/__init__.py
+++ HttpTriggerSAPKerUpg/__init__.py
@@ -2,18 +2,35 @@ import logging
 
 import azure.functions as func
 
+import paramiko
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+#    name = req.params.get('name')
+#    if not name:
+#        try:
+#            req_body = req.get_json()
+#        except ValueError:
+#            pass
+#        else:
+#            name = req_body.get('name')
+    command = "df"
+
+# Update the next three lines with your
+# server's information
+
+    host = "10.0.0.7"
+    username = "demouser"
+    password = "Demo@pass123"
+
+    client = paramiko.client.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(host, username=username, password=password)
+    _stdin, _stdout,_stderr = client.exec_command("df")
+    print(_stdout.read().decode())
+    name = _stdout.read().decode()
+    client.close()
 
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
