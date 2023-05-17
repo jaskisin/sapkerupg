3a51e05e9b904f694e2e7045b8f040ab770ce203
Jaskirat Singh <Jaskirat.Singh@microsoft.com>
2023-05-17 08:10:20 +0530

    testing download feature

--- HttpTriggerSAPKerUpg/__init__.py
+++ HttpTriggerSAPKerUpg/__init__.py
@@ -4,6 +4,12 @@ import azure.functions as func
 
 import paramiko
 
+import os
+
+import requests
+
+from urlparse import urlparse
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
@@ -15,27 +21,40 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 #            pass
 #        else:
 #            name = req_body.get('name')
-    command = "df"
+##    command = "df"
 
 # Update the next three lines with your
 # server's information
 
-    host = "10.0.0.7"
-    username = "demouser"
-    password = "Demo@pass123"
+##    host = "10.0.0.7"
+##    username = "demouser"
+##    password = "Demo@pass123"
 
-    client = paramiko.client.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(host, username=username, password=password)
-    _stdin, _stdout,_stderr = client.exec_command("touch abc")
-    print(_stdout.read().decode())
-    name = _stdout.read().decode()
-    client.close()
+##    client = paramiko.client.SSHClient()
+##    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+##    client.connect(host, username=username, password=password)
+##    _stdin, _stdout,_stderr = client.exec_command("touch abc")
+##    print(_stdout.read().decode())
+##    name = _stdout.read().decode()
+##    client.close()
+
+    username = 'S0024203723'
+    password = 'Voidmain@0'
+
+    url = 'https://softwaredownloads.sap.com/file/0020000000098642022'
+    filename = os.path.basename(urlparse(url).path)
+
+    r = requests.get(url, auth=(username,password))
+
+    if r.status_code == 200:
+        with open(filename, 'wb') as out:
+            for bits in r.iter_content():
+                out.write(bits)
 
 #    if name:
 #        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
 #    else:
     return func.HttpResponse(
-        f" {name} This HTTP triggered function executed successfully",
+        "This HTTP triggered function executed successfully",
         status_code=200
     )
