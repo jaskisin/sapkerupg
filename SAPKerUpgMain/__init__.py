bf6bc7e9ac89ec0921febb8623fc958fef07499a
Jaskirat Singh <Jaskirat.Singh@microsoft.com>
2023-05-20 20:03:55 +0530

    trying fix for errors

--- SAPKerUpgMain/__init__.py
+++ SAPKerUpgMain/__init__.py
@@ -22,26 +22,33 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         ospass = req_body.get('OperatingSystemPass')
         rootpass = req_body.get('RootPass')
         
-    def download_file_from_remote(server, loginid, loginpass, remotefile, localpath):
-        port = 22
-        transport = paramiko.Transport((server, port))
-        transport.connect(username = loginid, password = loginpass)
-        sftp = paramiko.SFTPClient.from_transport(transport)
-        sftp.get(remotefile, localpath)
-        sftp.close()
-        transport.close()
+    # def download_file_from_remote(server, loginid, loginpass, remotefile, localpath):
+        # port = 22
+        # transport = paramiko.Transport((server, port))
+        # transport.connect(username = loginid, password = loginpass)
+        # sftp = paramiko.SFTPClient.from_transport(transport)
+        # sftp.get(remotefile, localpath)
+        # sftp.close()
+        # transport.close()
         
-    def run_remote_command(server, loginid, loginpass, command):
-        try:
-            remotecommandclient = paramiko.client.SSHClient()
-            remotecommandclient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            remotecommandclient.connect(server, username=loginid, password=loginpass)
-            stdin, stdout, stderr = remotecommandclient.exec_command(command)
-            return str(stdout.read())
-        finally:
-            remotecommandclient.close()
+    # def run_remote_command(server, loginid, loginpass, command):
+        # try:
+            # remotecommandclient = paramiko.client.SSHClient()
+            # remotecommandclient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            # remotecommandclient.connect(server, username=loginid, password=loginpass)
+            # stdin, stdout, stderr = remotecommandclient.exec_command(command)
+            # return str(stdout.read())
+        # finally:
+            # remotecommandclient.close()
     
-    download_file_from_remote(host, osuser, ospass, "/usr/sap/sapservices", "/tmp/sapservices")
+    # download_file_from_remote(host, osuser, ospass, "/usr/sap/sapservices", "/tmp/sapservices")
+    
+    transport = paramiko.Transport((host, 22))
+    transport.connect(username = osuser, password = ospass)
+    sftp = paramiko.SFTPClient.from_transport(transport)
+    sftp.get("/usr/sap/sapservices", "/tmp/sapservices")
+    sftp.close()
+    transport.close()
     
     logging.info('Reading the file.')
     file1 = open('/tmp/sapservices', 'r')
@@ -52,8 +59,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             if "ASCS" in line:
                 sid=line.split("/")[3]
     
+    remote_command_client = paramiko.client.SSHClient()
+    remote_command_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    remote_command_client.connect(host, username=osuser, password=ospass)
+    stdin, stdout, stderr = remote_command_client.exec_command("chmod 777 /tmp/"+sapcarfile+" /tmp/"+sapexefile+";/tmp/"+sapcarfile+" -xvf /tmp/"+sapexefile+" -R /sapmnt/"+sid+"/exe/uc/linuxx86_64")
+    remote_command_client.close()
+    output = str(stdout.read())
+    
     #output = run_remote_command(host, osuser, ospass, "chmod 777 /tmp/"+sapcarfile+" /tmp/"+sapexefile+";/tmp/"+sapcarfile+" -xvf /tmp/"+sapexefile+" -R /sapmnt/"+sid+"/exe/uc/linuxx86_64")
     
     return func.HttpResponse(
-        f"{sid}",
+        f"{output}",
         status_code=200    )
